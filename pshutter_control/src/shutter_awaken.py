2ec31014bf724d8841206e23e71650fe93029ecc
k-przyk <keoni.spencer@yale.edu>
2022-12-01 11:08:59 -0500

    Awaken!

--- pshutter_control/src/shutter_awaken.py
+++ pshutter_control/src/shutter_awaken.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python
 
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import Float64MultiArray
 
 def wakeup_robot():
 
-    pub = rospy.Publisher('/unity_joint_group_controller/command', String)
-    # pub = rospy.Publisher('/joint_group_controller/command', String)
+    # pub = rospy.Publisher('/unity_joint_group_controller/command', Float64MultiArray)
+    pub = rospy.Publisher('/joint_group_controller/command', Float64MultiArray, queue_size=1)
 
-    rospy.init_node('awaken!!', anonymous=True)
+    rospy.init_node('awaken', anonymous=True)
+    timer = rospy.Rate(1)
 
-    while not rospy.is_shutdown():
-        joint_cmd = 
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        break;
+    i = 0;
+    while not rospy.is_shutdown() and i < 20:
+        joint_cmd = Float64MultiArray()
+        # joint_cmd.data = [0, 0, 0, 0]
+        joint_cmd.data = [0, -0.5, -0.2, -0.1]
+        pub.publish(joint_cmd)
+        timer.sleep()
+        i += 1
+        
 
 if __name__ == '__main__':
     try:
