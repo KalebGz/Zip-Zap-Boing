ffde10f59ef636adf2e7cb336640541940830c54
KalebGz <kaleb.zeray@gmail.com>
2022-12-15 05:25:59 -0500

    rosified speech transcriber

--- pshutter_control/src/transcribe_audio.py
+++ pshutter_control/src/transcribe_audio.py
@@ -1,238 +1,116 @@
+#!/usr/bin/env python
 from __future__ import division
 
 import re
 import sys
+import rospy
 
 from google.cloud import speech
 from google.oauth2 import service_account
 
-import pyaudio
-from six.moves import queue
+from std_msgs.msg import String
+from visualization_msgs.msg import MarkerArray
 
+
+from microphone_stream import MicrophoneStream          
 
 # Audio recording parameters
 RATE = 16000
-CHUNK = int(RATE / 100)  # 100ms
+CHUNK = int(RATE / 1000)  # 10ms
 
+class MoveTranscriber:
+    
+    def __init__(self):
+        rospy.init_node('move_transcriber', anonymous=False)
 
-class MicrophoneStream(object):
-    """Opens a recording stream as a generator yielding the audio chunks."""
+        self.move_pub = rospy.Publisher('/zzb_hear_move', String, queue_size=1)
+        self.rate = rospy.Rate(0.25)
 
-    def __init__(self, rate, chunk):
-        self._rate = rate
-        self._chunk = chunk
+        # TODO: Check if it makes more sense to  sub to zzb_move, look into concurrency issues ... 
+        self.sub = rospy.Subscriber('/body_tracking_data', MarkerArray, self.send_move, queue_size=1)
 
-        # Create a thread-safe buffer of audio data
-        self._buff = queue.Queue()
-        self.closed = True
+        rospy.spin()
 
-    def __enter__(self):
-        self._audio_interface = pyaudio.PyAudio()
-        self._audio_stream = self._audio_interface.open(
-            format=pyaudio.paInt16,
-            # The API currently only supports 1-channel (mono) audio
-            # https://goo.gl/z757pE
-            channels=1,
-            rate=self._rate,
-            input=True,
-            frames_per_buffer=self._chunk,
-            # Run the audio stream asynchronously to fill the buffer object.
-            # This is necessary so that the input device's buffer doesn't
-            # overflow while the calling thread makes network requests, etc.
-            stream_callback=self._fill_buffer,
+    def send_move(self, msg):
+
+        language_code = "en-US" 
+        credentials = service_account.Credentials.from_service_account_file("/Users/kaleb/Downloads/bim-project-371012-6b1b552052a5.json")
+        client = speech.SpeechClient(credentials=credentials)
+
+        config = speech.RecognitionConfig(
+            encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+            sample_rate_hertz=RATE,
+            language_code=language_code,
         )
 
-        self.closed = False
+        streaming_config = speech.StreamingRecognitionConfig(
+            config=config, interim_results=True
+        )
 
-        return self
+        with MicrophoneStream(RATE, CHUNK) as stream:
+            audio_generator = stream.generator()
+            requests = (
+                speech.StreamingRecognizeRequest(audio_content=content)
+                for content in audio_generator
+            )
 
-    def __exit__(self, type, value, traceback):
-        self._audio_stream.stop_stream()
-        self._audio_stream.close()
-        self.closed = True
-        # Signal the generator to terminate so that the client's
-        # streaming_recognize method will not block the process termination.
-        self._buff.put(None)
-        self._audio_interface.terminate()
+            responses = client.streaming_recognize(streaming_config, requests)
 
-    def _fill_buffer(self, in_data, frame_count, time_info, status_flags):
-        """Continuously collect data from the audio stream, into the buffer."""
-        self._buff.put(in_data)
-        return None, pyaudio.paContinue
+            # Now, put the transcription responses to use.
+            self.listen_print_loop(responses)
 
-    def generator(self):
-        while not self.closed:
-            # Use a blocking get() to ensure there's at least one chunk of
-            # data, and stop iteration if the chunk is None, indicating the
-            # end of the audio stream.
-            chunk = self._buff.get()
-            if chunk is None:
-                return
-            data = [chunk]
 
-            # Now consume whatever other data's still buffered.
-            while True:
-                try:
-                    chunk = self._buff.get(block=False)
-                    if chunk is None:
-                        return
-                    data.append(chunk)
-                except queue.Empty:
+    def listen_print_loop(self, responses):
+        """ Iterates through server responses and prints them. """
+
+        num_chars_printed = 0
+        for response in responses:
+            if not response.results:
+                continue
+
+            result = response.results[0] # Best Guess
+            if not result.alternatives:
+                continue
+
+            # Display the transcription of the top alternative.
+            transcript = result.alternatives[0].transcript.lower()
+
+            # Display interim results and  overwrite the previous result
+            overwrite_chars = " " * (num_chars_printed - len(transcript))
+            pub = ""
+            if not result.is_final:
+                sys.stdout.write(transcript + overwrite_chars + "\r")
+                sys.stdout.flush()
+
+                num_chars_printed = len(transcript)
+
+            else:
+                print(transcript + overwrite_chars)
+                # print("Transript: " + transcript  + "-----Overwrite Chars" + overwrite_chars)
+                if ("zip" in transcript or "is" in transcript or "set" in transcript or "ip" in transcript):
+                    # print("Guess: zip")
+                    pub = "zip"
+                elif ( "ap" in transcript or "op" in transcript or "at" in transcript):
+                    # print("Guess: zap")
+                    pub = "zap"
+                elif( "b" in transcript or  "ing" in transcript):
+                    # print("Guess: boing")
+                    pub = "boing"
+                else:
+                    # print("TERM NOT RECORGNIZED")
+                    pub = "None"
+
+                self.move_pub.publish(pub)
+
+                # Exit recognition if any of the transcribed phrases could be
+                # one of our keywords.
+                if re.search(r"\b(exit|quit)\b", transcript, re.I):
+                    print("Exiting..")
                     break
 
-            yield b"".join(data)
-
-
-def listen_print_loop(responses):
-    """Iterates through server responses and prints them.
-
-    The responses passed is a generator that will block until a response
-    is provided by the server.
-
-    Each response may contain multiple results, and each result may contain
-    multiple alternatives; for details, see https://goo.gl/tjCPAU.  Here we
-    print only the transcription for the top alternative of the top result.
-
-    In this case, responses are provided for interim results as well. If the
-    response is an interim one, print a line feed at the end of it, to allow
-    the next result to overwrite it, until the response is a final one. For the
-    final one, print a newline to preserve the finalized transcription.
-    """
-    num_chars_printed = 0
-    for response in responses:
-        if not response.results:
-            continue
-
-        # The `results` list is consecutive. For streaming, we only care about
-        # the first result being considered, since once it's `is_final`, it
-        # moves on to considering the next utterance.
-        result = response.results[0]
-        if not result.alternatives:
-            continue
-
-        # Display the transcription of the top alternative.
-        transcript = result.alternatives[0].transcript.lower()
-
-        # Display interim results, but with a carriage return at the end of the
-        # line, so subsequent lines will overwrite them.
-        #
-        # If the previous result was longer than this one, we need to print
-        # some extra spaces to overwrite the previous result
-        overwrite_chars = " " * (num_chars_printed - len(transcript))
-
-        if not result.is_final:
-            sys.stdout.write(transcript + overwrite_chars + "\r")
-            sys.stdout.flush()
-
-            num_chars_printed = len(transcript)
-
-        else:
-            print(transcript + overwrite_chars)
-            # print("Transript: " + transcript  + "-----Overwrite Chars" + overwrite_chars)
-            if ("zip" in transcript or "is" in transcript or "set" in transcript or "ip" in transcript):
-                print("Guess: zip")
-            elif ( "ap" in transcript or "op" in transcript or "at" in transcript):
-                print("Guess: zap")
-            elif( "b" in transcript or  "ing" in transcript):
-                print("Guess: boing")
-            else:
-                print("TERM NOT RECORGNIZED")
-            # Exit recognition if any of the transcribed phrases could be
-            # one of our keywords.
-            if re.search(r"\b(exit|quit)\b", transcript, re.I):
-                print("Exiting..")
-                break
-
-            num_chars_printed = 0
-
-def transcribe_with_model_adaptation(
-    project_id, location, storage_uri, custom_class_id, phrase_set_id):
-
-    """
-    Create`PhraseSet` and `CustomClasses` to create custom lists of similar
-    items that are likely to occur in your input data.
-    """
-
-
-
-def main():
-    # See http://g.co/cloud/speech/docs/languages
-    # for a list of supported languages.
-    language_code = "en-US"  # a BCP-47 language tag
-
-    # client = speech.SpeechClient()
-    # client = language.LanguageServiceClient.from_service_account_json("/Users/kaleb/Downloads/bim-project-371012-6b1b552052a5.json")
-    
-    credentials = service_account.Credentials.from_service_account_file("/Users/kaleb/Downloads/bim-project-371012-6b1b552052a5.json")
-    client = speech.SpeechClient(credentials=credentials)
-
-                    # Speech adaptation configuration
-    #     # Create the adaptation client
-    # adaptation_client = speech.AdaptationClient()
-
-    # # The parent resource where the custom class and phrase set will be created.
-    # parent = f"projects/{project_id}/locations/{location}"
-
-    # # Create the custom class resource
-    # adaptation_client.create_custom_class(
-    #     {
-    #         "parent": parent,
-    #         "custom_class_id": custom_class_id,
-    #         "custom_class": {
-    #             "items": [
-    #                 {"value": "sushido"},
-    #                 {"value": "altura"},
-    #                 {"value": "taneda"},
-    #             ]
-    #         },
-    #     }
-    # )
-    # custom_class_name = (
-    #     f"projects/{project_id}/locations/{location}/customClasses/{custom_class_id}"
-    # )
-    # # Create the phrase set resource
-    # phrase_set_response = adaptation_client.create_phrase_set(
-    #     {
-    #         "parent": parent,
-    #         "phrase_set_id": phrase_set_id,
-    #         "phrase_set": {
-    #             "boost": 10,
-    #             "phrases": [
-    #                 {"value": f"Visit restaurants like ${{{custom_class_name}}}"}
-    #             ],
-    #         },
-    #     }
-    # )
-    # phrase_set_name = phrase_set_response.name
-    # speech_adaptation = speech.SpeechAdaptation(phrase_set_references=[phrase_set_name])
-
-
-    config = speech.RecognitionConfig(
-        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=RATE,
-        language_code=language_code,
-        
-    )
-        # adaptation=speech_adaptation,
-
-
-    streaming_config = speech.StreamingRecognitionConfig(
-        config=config, interim_results=True
-    )
-
-    with MicrophoneStream(RATE, CHUNK) as stream:
-        audio_generator = stream.generator()
-        requests = (
-            speech.StreamingRecognizeRequest(audio_content=content)
-            for content in audio_generator
-        )
-
-        responses = client.streaming_recognize(streaming_config, requests)
-
-        # Now, put the transcription responses to use.
-        listen_print_loop(responses)
+                num_chars_printed = 0
 
 
 if __name__ == "__main__":
     print("STARTING TO LISTEN")
-    main()
+    transcriber = MoveTranscriber()
